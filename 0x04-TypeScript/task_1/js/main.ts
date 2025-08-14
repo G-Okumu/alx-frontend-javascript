e3a13f319a08054858f5eb611de58323d6a7cfd0
George-Okumu <gokumu12@gmail.com>
2025-08-14 06:41:05 +0300

    writting a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,16 +7,42 @@ interface Teacher {
   [key: string]: any; // Allow any other properties like `contract`
 }
 
-
 interface Director extends Teacher {
-  numberOfReports: number
+  numberOfReports: number;
 }
 
 function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }) {
   return `${firstName}. ${lastName}`;
 }
 
-
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
+}
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  public firstName: string;
+  public lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
