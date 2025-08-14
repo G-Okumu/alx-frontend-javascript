301d096e40fee06ed83203426434e8b42e4452e4
George-Okumu <gokumu12@gmail.com>
2025-08-14 19:21:20 +0300

    pass test

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,9 +2,9 @@
 
 namespace Subjects {
   export class Subject {
-    protected teacher: Teacher;
+    protected teacher:Teacher;
 
-    setTeacher(teacher: Teacher): void {
+    setTeacher(teacher:Teacher): void {
       this.teacher = teacher;
     }
   }
