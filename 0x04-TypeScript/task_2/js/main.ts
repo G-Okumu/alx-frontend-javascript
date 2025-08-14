333a5f25995ab5e029f9bd7f926f35f5b99bf0db
George-Okumu <gokumu12@gmail.com>
2025-08-14 18:54:41 +0300

    pass test

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,14 @@ function executeWork(employee: Director | Teacher): string {
         return employee.workTeacherTasks();
     }
 }
+
+
+type Subjects = "Math" | "History";
+
+export function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    } else {
+        return "Teaching History";
+    }
+}
