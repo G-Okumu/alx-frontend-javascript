05fcce20ff797170e6875906703991e3fa0832a7
George-Okumu <gokumu12@gmail.com>
2025-08-14 19:25:42 +0300

    new export class

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,38 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import Cpp = Subjects.Cpp;
+import Java = Subjects.Java;
+import React = Subjects.React;
+import Teacher = Subjects.Teacher;
+
+// Create and export subject instances
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+// Create and export teacher object with C experience only
+export const cTeacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
+};
+
+// C++ Subject
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java Subject
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React Subject
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
